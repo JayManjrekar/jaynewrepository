bdcad07f480626f11a739810f878362be5daa863
Karthik Valluri <72475804+kar722@users.noreply.github.com>
2022-04-01 23:51:01 +0000

    palindrome re

--- Week2/palindromewre.py
+++ Week2/palindromewre.py
@@ -1,39 +1,4 @@
-# palindrome with re
 
-import re
-
-
-class Palindrome:
-    def __init__(self, candidate):
-      self._candidate = candidate 
-      self._length = len(candidate)   
-      self._is_a_palindrome = False 
-    
-      self._az09 = re.sub(r'[^a-zA-Z0-9]', '', self._candidate)  
- 
-      self._analysis = []  
-      self._tests = 0 
-  
-      self.__call__()  
-
-    def __call__(self):
-      c = self._az09
-      tests = int(len(c) / 2)
-    
-      for i in range(0, tests):
-          front = c[i];
-          back = c[len(c) - i - 1]
-          if front.lower() == back.lower():
-              self.logger(front, back, True)
-              self._tests += 1
-          else:
-              self.logger(front, back, False)
-              return
-      self._is_a_palindrome = True
-      return
-
-    def logger(self, front, back, result):
-      self._analysis.append({"test": self._tests, "front": front, "back": back, "result": result})
 
       
 
